cc3e6854a3f6ac2c8ddd68dd3e529f52e4e884d6
Romain Lanz <romain.lanz+github@pm.me>
2024-04-25 20:29:42 +0200

    chore: latest update

--- app/controllers/chats_controller.ts
+++ app/controllers/chats_controller.ts
@@ -1,9 +1,16 @@
 import transmit from '@adonisjs/transmit/services/main'
 import type { HttpContext } from '@adonisjs/core/http'
+import {DateTime} from 'luxon'
 
 export default class ChatsController {
   store({ request, response }: HttpContext) {
-    transmit.broadcast('chats/1', { message: request.input('message') })
+    const { message, username } = request.only(['message', 'username'])
+
+    if (!message) {
+      return response.redirect().back()
+    }
+
+    transmit.broadcast('chats/1', { message: `[${DateTime.now().toFormat('DD H:mm:ss')}] ${username ?? 'Guest'}: ${message}` })
 
     return response.redirect().back()
   }
--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ import { Transmit } from '@adonisjs/transmit-client'
 
 const messageContainer = document.querySelector('#messages')
 const messageForm = document.querySelector('#message_form')
+const messageInput = document.querySelector('#message')
 
 messageForm.addEventListener('submit', (e) => {
   e.preventDefault()
@@ -9,11 +10,11 @@ messageForm.addEventListener('submit', (e) => {
   const values = new FormData(e.currentTarget)
   void fetch('/messages', { method: 'POST', body: values })
 
-  e.currentTarget.reset()
+  messageInput.value = ''
 })
 
 const transmit = new Transmit({
-  baseUrl: 'http://localhost:3333',
+  baseUrl: window.location.origin,
 })
 
 const subscription = transmit.subscription('chats/1')
